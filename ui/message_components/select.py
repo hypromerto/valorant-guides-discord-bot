f03d3311bfdb5e45ab7b3dd7460d655a7e0e304c
hypromerto <aslan-mert99@hotmail.com>
2022-08-26 20:40:55 +0300

    Add previous choices notifications

--- ui/message_components/select.py
+++ ui/message_components/select.py
@@ -1,5 +1,7 @@
 import discord.ui
 
+from ui.state_machine import previous_states_of_state
+
 
 class Select(discord.ui.Select):
 
@@ -9,6 +11,19 @@ class Select(discord.ui.Select):
         self.key = key
 
     async def callback(self, interaction: discord.Interaction):
-        await interaction.message.edit(view=self.view.update_view(self.domain_type, self.key, self.values[0]))
+
+        previous_choices = self.key.split('_')
+
+        content_message = ''
+
+        previous_states = previous_states_of_state(self.domain_type)
+
+        for index, previous_state in enumerate(previous_states):
+            content_message += f'**{previous_state.name.capitalize()}:**   {previous_choices[index]}\n'
+
+        content_message += f'**{self.domain_type.capitalize()}:**   {self.values[0]}\n'
+
+        await interaction.message.edit(content=content_message,
+                                       view=self.view.update_view(self.domain_type, self.key, self.values[0]))
 
         await interaction.response.defer()
--- ui/state_machine.py
+++ ui/state_machine.py
@@ -12,3 +12,35 @@ def calculate_state_transitions_for_guides(current_state):
         return DomainType.side
     elif current_state == DomainType.side.name:
         return DomainType.area
+
+def previous_states_of_state(current_state):
+
+    all_states = [DomainType.agent, DomainType.map, DomainType.guide, DomainType.ability, DomainType.side, DomainType.area]
+
+    if current_state == DomainType.area.name:
+        return all_states[:-1]
+
+    all_states.pop()
+
+    if current_state == DomainType.side.name:
+        return all_states[:-1]
+
+    all_states.pop()
+
+    if current_state == DomainType.ability.name:
+        return all_states[:-1]
+
+    all_states.pop()
+
+    if current_state == DomainType.guide.name:
+        return all_states[:-1]
+
+    all_states.pop()
+
+    if current_state == DomainType.map.name:
+        return all_states[:-1]
+
+    all_states.pop()
+
+    if current_state == DomainType.agent.name:
+        return all_states[:-1]
