a23f4ffad43adf85d5b2e2066c16b5b1af85869c
mert <aslan-mert99@hotmail.com>
2022-08-28 00:58:55 +0300

    Implement S3 client

---
+++ client/s3_client.py
@@ -0,0 +1,39 @@
+import io
+
+import boto3
+
+
+class S3Client:
+
+    def __init__(self, access_key_id, secret_access_key):
+        self.bucket_name = 'valorant-guides-bot'
+        self.s3_client = boto3.client('s3', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
+
+    def get_all_options(self, query):
+        options = []
+
+        folder_result = self.s3_client.list_objects_v2(Bucket=self.bucket_name, Prefix=query, Delimiter='/')
+
+        for prefix in folder_result['CommonPrefixes']:
+            options.append(prefix['Prefix'][:-1].rsplit('/', 1)[-1])
+
+        return options
+
+    def download_all_objects(self, query_dir):
+        files = []
+
+        buf = io.BytesIO()
+
+        obj_results = self.s3_client.list_objects_v2(Bucket=self.bucket_name, Prefix=query_dir, Delimiter='/')
+
+        for obj in obj_results['Contents']:
+
+            if obj['Key'] == query_dir:
+                continue
+
+            self.s3_client.download_fileobj(self.bucket_name, obj['Key'], buf)
+            file_content = io.BytesIO(buf.getvalue())
+
+            files.append(file_content)
+
+        return files
