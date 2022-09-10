2b2d83a7fcf549f4a3b6d9f9ac3cef7937b35e9d
hypromerto <aslan-mert99@hotmail.com>
2022-09-10 22:37:09 +0300

    Add region to boto3 logger client

--- infra/config/logger.py
+++ infra/config/logger.py
@@ -1,5 +1,6 @@
 import logging
 
+import boto3
 import watchtower
 
 
@@ -18,8 +19,9 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("root")
 
 dt_fmt = '%d-%m-%Y %H:%M:%S'
+logs_client = boto3.client("logs", region_name="eu-central-1")
 
-handler = watchtower.CloudWatchLogHandler(log_group_name="valorant-guides-bot", log_stream_name="ec2-stream")
+handler = watchtower.CloudWatchLogHandler(log_group_name="valorant-guides-bot", log_stream_name="ec2-stream", boto3_client=logs_client)
 
 formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
 
